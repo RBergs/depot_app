ac2f6b89edd6499267e97b74454f816f593bb047
Richard Berger <richard.h.berger@gmail.com>
2011-12-19 23:22:28 -0500

    removed confirmation emails

--- config/environment.rb
+++ config/environment.rb
@@ -4,16 +4,16 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Depot::Application.initialize!
 
-Depot::Application.configure do
-  config.action_mailer.delivery_method = :smtp
+# Depot::Application.configure do
+# config.action_mailer.delivery_method = :smtp
 	
-config.action_mailer.smtp_settings = {
-	 	:address							=> "smtp.gmail.com",
-	 	:port									=> 587,
-	 	:domain								=> "rich-bookstore.herokuapp.com",
-	 	:authentication 			=> "plain",
-	 	:user_name						=> "Rich",
-	 	:password							=> "wedges",
-	 	:enable_starttls_auto => true
-	 }
- end
+# config.action_mailer.smtp_settings = {
+#	 	:address							=> "smtp.gmail.com",
+#	 	:port									=> 587,
+#	 	:domain								=> "rich-bookstore.herokuapp.com",
+#	 	:authentication 			=> "plain",
+#	 	:user_name						=> "Rich",
+#	 	:password							=> "secret",
+#	 	:enable_starttls_auto => true
+#	 }
+# end
